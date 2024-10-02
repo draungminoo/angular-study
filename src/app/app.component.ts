d14c9597be06de1d3786f0149329f4ed8301c730
draungminoo <minooaung28@gmail.com>
2024-10-02 11:48:02 +0630

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type OptionsType = {
+  id: number;
+  name: string;
+};
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-study';
+  title = 'This is title';
+  body = 'this is body';
+  doubleQuotes = 'This is Apple.';
+
+  // In App Methods
+  // #region
+
+  // add two numbers
+  addTwoNumbers(a: number, b: number) {
+    return a + b;
+  }
+
+  /**
+   * What this method do?
+   * @param options OptionsType
+   * @returns string
+   */
+  documentationTest(options: OptionsType) {
+    return options.name;
+  }
+
+  // #endregion
+
+  // Server Methods
+  // #region
+
+  /**
+   * To take student data from the server.
+   */
+  getDataFromServer() {}
+
+  getDataFromServer2() {}
+
+  getDataFromServer3() {}
+
+  getDataFromServer4() {}
+
+  getDataFromServer5() {}
+
+  getDataFromServer6() {}
+
+  getDataFromServer7() {}
+
+  // #endregion
 }
